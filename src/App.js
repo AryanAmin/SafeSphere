d91f1b44cc79744e538b9a43e8456a5c4c5fdb4c
Vaibhav Patel <vaibhav.p2305@gmail.com>
2023-06-24 03:48:24 -0400

    feature(filestructure):Added file structure to the code base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
