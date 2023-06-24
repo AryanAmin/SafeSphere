55c2163ae62b0f57ad975c68a6e523358b64bb4e
Rootul Patel <patelrootul1@gmail.com>
2023-06-24 12:14:45 -0400

    feature (loginpage) loginpage and post merged

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./Home.css";
-// import { useStateValue } from "../../StateProvider";
 import Posts from "../posts/posts";
 import PostDetails from "../postDetails/postDetails";
 import { useNavigate } from "react-router";
+import { useStateValue } from "../../StateProvider";
 
 const posts = [
   {
@@ -21,9 +21,18 @@ const posts = [
 ];
 
 function Home() {
+  const [{user}] = useStateValue();
+  const history = useNavigate();
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const handlePostClick = (e, postCid) => {
+    e.preventDefault();
+    history(`/post/${postCid}`)
+  };
+
   return (
     <div className="home-container">
-      {/* {user} */}
+      {user}
       <h1>Main Feed</h1>
         <Posts posts={posts} onPostClick={handlePostClick} />
     </div>
