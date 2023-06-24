11ffa796cd5f8275e569cf0db75f7f164420ce8d
Vaibhav Patel <vaibhav.p2305@gmail.com>
2023-06-24 04:50:49 -0400

    feature(filestructure): Working on the homepage ui elements

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="navigation-container">
+      <h1>Navigation</h1>
+    </div>
+  );
+}
+
+export default Navigation;
