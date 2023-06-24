72b51e6a078802fae3021c0798d3a3164262955d
durga256 <durgadevi256@gmail.com>
2023-06-24 07:17:01 -0400

    feature (profile page) added native token price fetch function

--- src/components/profilepage/profilepage.js
+++ src/components/profilepage/profilepage.js
@@ -12,10 +12,10 @@ function fetchUserCoinBalance(userAddress){
     let coin_balance = []
     let url = '';
     for(let i = 0; i < coins_list.length; i++){
-        url = 'https://deep-index.moralis.io/api/v2/'+userAddress+'/balance?chain='+i;
+        url = 'https://deep-index.moralis.io/api/v2/'+userAddress+'/balance?chain='+coins_list[i];
         axios.get(url, config)
             .then(res=> {
-                coin_balance.append([i,res.balance])
+                coin_balance.append([coins_list[i],res.balance])
             })
             .catch(err=> console.log(err))
     }
@@ -23,6 +23,7 @@ function fetchUserCoinBalance(userAddress){
 }
 
 function fetchUserERC20Balance(userId){
+    const axios = require('axios');
     const url = 'https://deep-index.moralis.io/api/v2/'+userId+'/erc20?chain=eth';
     let coin_balance = [];
     axios.get(url, config)
@@ -35,10 +36,50 @@ function fetchUserERC20Balance(userId){
     return coin_balance;
 }
 
+function fetchTokenPrice(userBalance){
+    const axios = require('axios');
+    let url = '';
+    let coin_balance = [];
+    const config = {
+        headers:{
+            'accept': 'application/json',
+            'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImUyMGViYWVlLWJhZDEtNDBjNy1iNjNlLTMxYjRkY2I2MTMwOSIsIm9yZ0lkIjoiMzQ1MDc1IiwidXNlcklkIjoiMzU0NzMyIiwidHlwZUlkIjoiNjE3NjI2NWYtNjJlZS00ODA5LTgxOTYtMDAyNmZmMWQxOWY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODc1ODcwNjcsImV4cCI6NDg0MzM0NzA2N30.Kozfsy8I9k-kTM3YnkSUKBJ_TmMbDHUruvIhyzhSPvk'
+        }
+      };
+    for(let i = 0; i < userBalance.length; i++){
+        url = 'https://deep-index.moralis.io/api/v2/erc20/'+userBalance[i][1]+'/price?chain=eth';
+        axios.get(url, config)
+            .then(res=> {
+                coin_balance.append([userBalance[i][0],res.usdPrice])
+            })
+            .catch(err=> console.log(err))
+    }
+    return coin_balance;
+}
+
+function fetchNativeTokenPrice(){
+    const axios = require('axios');
+    const coins_list = ['ethereum', 'matic-network'];
+    const coins_list_moralis = ['eth', 'matic'];
+    let url = '';
+    let coin_balance = [];
+    for(let i = 0; i < coins_list.length; i++){
+        url = 'https://api.coingecko.com/api/v3/simple/price?ids='+coins_list[i]+'&vs_currencies=usd';
+        axios.get(url)
+            .then(res=> {
+                coin_balance.append([coins_list_moralis[i],res[coins_list[i]['usd']]]);
+            })
+            .catch(err=> console.log(err))
+    }
+    return coin_balance;
+}
+
 export default function profilepage(){
     const userId = useParams();
     const userBalance = fetchUserCoinBalance(userId);
     const userERC20Balance = fetchUserERC20Balance(userId);
+    const ERC20TokenPrice = fetchTokenPrice(userERC20Balance);
+    const nativeTokenPrice = fetchNativeTokenPrice(userId);
     
     return ();
 }
