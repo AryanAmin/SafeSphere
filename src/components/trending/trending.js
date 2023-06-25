262277347a843300e2dac31bb9ede7d7f3e6de49
AryanAmin1902 <amin5300@mylaurier.ca>
2023-06-25 03:05:56 -0400

    Garbage code

--- src/components/trending/trending.js
+++ src/components/trending/trending.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./Trending.css";
-import { useNavigate } from "react-router";
 import {
   useGetTokenBalances,
   useLazyQueryWithPagination,
@@ -8,31 +7,17 @@ import {
 } from "@airstack/airstack-react";
 import { useStateValue } from "../../StateProvider";
 import { queryForPieChart, queryForTrending } from "../../queries/queries";
-import NFTList from "./NFTList";
-import DappFilter from "./DappFilter";
-import Navigation from "../navigation/Navigation";
 
 function Trending() {
-  const history = useNavigate();
-  const [dapp, setDapp] = useState('opensea');
-  const { data, loading, error } = useQuery(queryForTrending(dapp));
+  const { data, loading, error } = useQuery(queryForTrending("opensea"));
   const [{ user }] = useStateValue();
   const [trendingTokens, setTrendingTokens] = useState([]);
-
-  const onChangeDapp = (selectedDapp) => {
-    setDapp(selectedDapp);
-}
-
-const onSubmitHandler = (e) => {
-  e.preventDefault();
-  history(`/profile/${e.target.fname.value}`);
-}
 
   useEffect(() => {
     if (data) {
       const { trending } = data;
       const trendingCollection = trending?.CollectionStat || [];
-      setTrendingTokens(trendingCollection);
+      setTrendingTokens((tokens) => [...tokens, ...trendingCollection]);
     }
   }, [data]);
 
@@ -41,25 +26,8 @@ const onSubmitHandler = (e) => {
   }, [trendingTokens]);
 
   return (
-    <div class="wrapper-trending">
-    <div class="nav-bar">
-        <Navigation />
-    </div>
-    <div class="search-bar-div">
-        <form onSubmit={onSubmitHandler}>
-            <input class="search-bar"type="text" id="fname" name="fname" />
-            <input type="submit" value="Submit" />
-        </form>
-    </div>
-    <div class="trending-body">
-        <div class='filter-bar'>
-        <DappFilter selected={dapp} onChangeFilter={onChangeDapp}/>
-        </div>
-        <div class='filtered-nfts'>
-            <NFTList items={trendingTokens}/>
-        </div>
-
-    </div>
+    <div className="home-container">
+      <h1>Trending</h1>
     </div>
   );
 }
